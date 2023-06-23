60e17832855b393c0b4c4699bb850bfb882f08d2
Arslan Matloob <raoarslan263@gmail.com>
2023-06-24 00:16:53 +0500

    Tic tac toe Complete

--- tictactoe.js
+++ tictactoe.js
@@ -1,32 +1,32 @@
 let playerSymbol = "X";
-let gameEnded = false
+let gameEnded = false;
 
 let winPos = [
-  [1, 2, 3], [4, 5, 6], 
-  [7, 8, 9], [1, 4, 7], 
-  [2, 5, 8], [3, 6, 9], 
-  [1, 5, 9], [3, 5, 7]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
 ];
 
 for (let i = 1; i <= 9; i++) {
-  document.getElementById(i.toString()).addEventListener(
-    "click", 
-    function() {
-      if (this.innerHTML === "" && !gameEnded) {
-        this.innerHTML = playerSymbol;
-        
-        checkWin();
-        if(playerSymbol === "X"){
-          playerSymbol = "O"
-          this.classList.add('p1');
-          
-        }else{
-          playerSymbol = "X";
-          this.classList.add('p2');
-        }
+  document.getElementById(i.toString()).addEventListener("click", function () {
+    if (this.innerHTML === "" && !gameEnded) {
+      this.innerHTML = playerSymbol;
+
+      checkWin();
+      if (playerSymbol === "X") {
+        playerSymbol = "O";
+        this.classList.add("p1");
+      } else {
+        playerSymbol = "X";
+        this.classList.add("p2");
       }
     }
-  );
+  });
 }
 
 res = document.querySelector(".playerOne");
@@ -34,36 +34,48 @@ res2 = document.querySelector(".playerTwo");
 
 function checkWin() {
   for (let i = 0; i < winPos.length; i++) {
-	if (
-	  document.getElementById(winPos[i][0]).innerHTML === playerSymbol &&
-	  document.getElementById(winPos[i][1]).innerHTML === playerSymbol &&
-	  document.getElementById(winPos[i][2]).innerHTML === playerSymbol
-	) {
-	  document.getElementById(winPos[i][0]).classList.add("win");
-	  document.getElementById(winPos[i][1]).classList.add("win");
-	  document.getElementById(winPos[i][2]).classList.add("win"); 
-    
-	  gameEnded = true;
-	  setTimeout(function() {
-      if(playerSymbol =="O"){
-        res.classList.add('show');
-      }else{
-        res2.classList.add('show');
-      }
-	   
-	  }, 500);
-	}
+    if (
+      document.getElementById(winPos[i][0]).innerHTML === playerSymbol &&
+      document.getElementById(winPos[i][1]).innerHTML === playerSymbol &&
+      document.getElementById(winPos[i][2]).innerHTML === playerSymbol
+    ) {
+      document.getElementById(winPos[i][0]).classList.add("win");
+      document.getElementById(winPos[i][1]).classList.add("win");
+      document.getElementById(winPos[i][2]).classList.add("win");
+      gameEnded = true;
+      setTimeout(function () {
+        if (playerSymbol == "X") {
+          res.classList.add("show");
+          winings();
+        } else {
+          res2.classList.add("show");
+          winings();
+        }
+      }, 500);
+    }
   }
 }
 
-  function reset() {
-    for (let i = 1; i <= 9; i++) {
-      document.getElementById(i.toString()).innerHTML = "";
-      document.getElementById(i.toString()).classList.remove("x");
-      document.getElementById(i.toString()).classList.remove("o");
-      document.getElementById(i.toString()).classList.remove("win");
-      gameEnded = false;
-      res.classList.remove("show");
-      res2.classList.remove("show");
-    }
+function reset() {
+  for (let i = 1; i <= 9; i++) {
+    document.getElementById(i.toString()).innerHTML = "";
+    document.getElementById(i.toString()).classList.remove("x");
+    document.getElementById(i.toString()).classList.remove("o");
+    document.getElementById(i.toString()).classList.remove("win");
+    gameEnded = false;
+    res.classList.remove("show");
+    res2.classList.remove("show");
   }
+}
+
+var Xwinnings = 0;
+var Owinnings = 0;
+function winings() {
+  if (playerSymbol === "O") {
+    Xwinnings += 1;
+    document.getElementById("playerOneWins").innerHTML = Xwinnings;
+  } else {
+    Owinnings += 1;
+    document.getElementById("playerTwoWins").innerHTML = Owinnings;
+  }
+}
